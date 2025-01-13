3728e42b3e1dda5edd7708d832e5379c29b4b62d
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2025-01-13 13:03:25 +0545

    todo with mongoodb

--- app.js
+++ app.js
@@ -4,15 +4,40 @@ const path = require('path')
 const  {createClient}   = require('@supabase/supabase-js')
 const { error } = require('console')
 const { ifError } = require('assert')
-const { devNull } = require('os')
+const { devNull, type } = require('os')
 
+const mongoose = require('mongoose')
 
-const supabase = createClient('https://gszowrbrsqeeaaqjzuit.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdzem93cmJyc3FlZWFhcWp6dWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY1MTk5NTUsImV4cCI6MjA1MjA5NTk1NX0.33dSkbZAI8gP5ZOoRTyfPJtONVPf4FHrC5orHJVF33E')
+try {
+  
 
+  mongoose.connect('mongodb+srv://root:sspear54321@todos.umqh6.mongodb.net/?retryWrites=true&w=majority&appName=todos')
 
+   console.log("Conected mongodb")
 
+} catch (error) {
+  console.log("Err", error)
+  
+}
 
-const fs = require('fs').promises
+const todoSchema = new mongoose.Schema({
+
+title :{
+  type : String
+},
+
+note :{
+  type : String
+},
+
+completed : {
+  type : Boolean
+}
+
+})
+
+  const Todos = mongoose.model('todos', todoSchema)
+
 const port = 5000
 
 
@@ -20,156 +45,104 @@ const app = express()
 
 app.use(express.json()) //body parser
  
-const filepath = path.join(__dirname, 'todo.json')
- 
-const readTodos = async() =>{
-
- try {
-     
-    const data =  await fs.readFile(filepath, 'utf-8')
-    return JSON.parse(data) 
-
- } catch (error) {
-  
-  console.error('Error reading file')
-  return []
- }
-
-}
-
-const writeTodos = async(todos) =>{
-
-try {
-    
-  await fs.writeFile(filepath, JSON.stringify(todos, null, 2))
-  return []
 
 
-} catch (error) {
-  console.error("Error writing file", error)
-}
-
-}
 
 app.get('/todos' , async(req, res)=>{
  
-
-  // const {completed} = req.query
-  //  const todos = await readTodos()
-
-  // if (completed !== undefined) {
-    
-  //    const data =  todos.filter((t)=>t.completed.toString() === completed)
-  //    return  res.send({todos: data})
-  //   }
-  console.log("Hello")
-  const {data, error} = await supabase.from('todo').select('*')
-      console.log(data)
-     
+    try {
       
-  if(error){
-    return res.status(400).json({error: error.message})
-  }
-  res.send({
-    data
-    
-  })
+      const todos = await Todos.find()
+
+      res.status(200).send(todos)
+
+    } catch (error) {
+      res.status(404).send('err', error)
+    }
 
 })
 
 app.get('/todos/:id',  async(req, res)=>{
       const {id} = req.params 
+ 
+       try {
+        
+       const todos  =   await Todos.findById(id)     
+     res.status(200).send(todos)
 
-      // const todos = await readTodos()
-
-        const   {data:todos, error} = await supabase.from('todo').select('*').eq('id', id)
-       
-      // const todo =  todos.find((current)=>current.id === Number(id))
-       
-     if(error){
-       res.status(404).send({message: error})
-     }
-       
-      res.status(200).send(todos)
+       } catch (error) {
+        res.status(404).send("Error", error)
+       }
+   
 })
 
 app.post('/todos', async(req, res)=>{
 
-const {title, note}= req.body
+const {title, note, completed}= req.body
 
+ 
 
-if(!title){
- return  res.status(400).send({message: "Title cannot be empty"})
-}
-// const todos = await readTodos()
+ const todos = new Todos({
 
-
-const newTodo = {id : Date.now(), title: title, note: note, completed: false}
-
-
-const {error} = await supabase.from('todo').insert({title: newTodo.title, note: newTodo.note, completed:newTodo.completed})
-
-// todos.push(newTodo)
-
-// await writeTodos(todos)
-
-res.send("Todo created")
+  title,
+  note,
+  completed
 })
+
+    await todos.save()   // saved a data to the database
+   res.status(201).send(`todo created ${todos}`)
+})
+
 
 app.put('/todos/:id', async(req, res)=>{
 
 const {id} = req.params
+const {title, note} = req.body
 
-  // const todos =  await readTodos()
+      const updatedTodo  =  await Todos.findByIdAndUpdate(
+        id, 
+        {
+          title:title,
+          note : note
+        }
+      )
 
-    const todo = todos.find((current)=>current.id === Number(id))
-      
-    if(!todo){
-      res.send(404).send("Not found")
-    }
-    
-    todo.completed = true
-      
-    // writeTodos(todos)
-    res.send("Updated")
+     if(!updatedTodo){
+
+     return res.send(201).send("Not found")
+     }
+
+     res.status(200).send(`Todo updated ${updatedTodo}`)
 
 })
 
 app.delete('/todos/:id', async(req, res)=>{
 
      const {id} = req.params
-          //  let todos = await readTodos()
-       
-      //  let todo = todos.find((current)=>current.id === Number(id))
-                
-      const   {data:todos, error} = await supabase.from('todo').delete('*').eq('id', id)
-       
-       if(error){
-         res.status(404).send({message:error})
+       try {
+        await Todos.findByIdAndDelete(id)
+        console.log("Successfully deleted")
+        res.send("Deleted successfully")
+     
+       } catch (error) {
+         res.status(500).send(error)
        }
-       
-        // todos = todos.filter((c)=>c.id !== todo.id)
 
-        // await writeTodos(todos)
-
-        res.status(200).send({message : `todo with id deleted successfully`})
 })
 
 app.delete('/todos' , async(req, res)=>{
- 
-      // const todos = await  readTodos()
+      
+  try {
 
-    const {data , error} =  await supabase.from('todo').delete().neq('id', 0)
-          
-    // .neq('completed', false)
-      // await writeTodos([])
+    const todoDeleted =  await Todos.deleteMany({})
+    console.log(todoDeleted)
+    res.status(200).send("Todo deleted")
     
-      if(error){
+  } catch (error) {
+    res.send("Error", error)
+  }
+    
 
-        return res.status(400).json({message: error})
-      }
-
- res.status(200).send(`Todo deleted successfully`)
     
 })
 
